bc7039d8782dd138194b5f142e1049a33a518228
Peter <ghdtjrgus333@naver.com>
2022-08-22 09:47:35 +0900

    상세 뷰에 들어갈 항복 라벨 추가

--- MovieRanker/MovieRanker/Scenes/MovieDetailViewController.swift
+++ MovieRanker/MovieRanker/Scenes/MovieDetailViewController.swift
@@ -6,16 +6,75 @@
 //
 
 import UIKit
-import SnapKit
+import Kingfisher
+import PinLayout
 
 final class MovieDetailViewController: UIViewController, MovieDetailProtocol {
     private var presenter: MovieDetailPresenter!
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
     private lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.clipsToBounds = true
         imageView.contentMode = .scaleAspectFit
         imageView.backgroundColor = .lightGray
         return imageView
+    }()
+    
+    private lazy var rankingLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        label.text = "평점"
+        return label
+    }()
+    
+    private lazy var rankingDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    private lazy var actorLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        label.text = "배우"
+        return label
+    }()
+    
+    private lazy var actorDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    private lazy var directorLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        label.text = "감독"
+        return label
+    }()
+    
+    private lazy var directorDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    private lazy var movieMakingYearLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        label.text = "제작년도"
+        return label
+    }()
+    
+    private lazy var movieMakingYearDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        return label
     }()
     
     init(movie: Movie) {
@@ -40,6 +99,7 @@ extension MovieDetailViewController {
         view.backgroundColor = .systemBackground
         
         navigationItem.title = movie.title
+        imageView.kf.setImage(with: movie.imageURL)
         
         layout()
     }
@@ -48,10 +108,8 @@ extension MovieDetailViewController {
     private func layout() {
         [imageView]
             .forEach { self.view.addSubview($0) }
-        
-        imageView.snp.makeConstraints { make in
-            make.top.left.right.equalTo(view.safeAreaLayoutGuide).inset(16)
-            make.height.equalTo(imageView.snp.width)
-        }
+        imageView.sizeToFit()
+        imageView.pin.top(self.view.pin.safeArea).left().right().marginTop(16).marginHorizontal(16)
+            .height(imageView.frame.width)
     }
 }
